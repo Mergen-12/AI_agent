7da3baa5c871cda112862c25605fe438d77b15dc
mergen <70312323+Mergen-12@users.noreply.github.com>
2025-01-17 22:31:51 +0800

    Update __gui_style.py

--- __gui_style.py
+++ __gui_style.py
@@ -1,13 +1,7 @@
 from PyQt5.QtWidgets import (
-    QApplication, 
-    QMainWindow, 
-    QPushButton, 
-    QVBoxLayout, 
-    QHBoxLayout, 
-    QWidget, 
     QLineEdit, 
     QTextEdit,
-    QFrame
+    QPushButton
 )
 
 # Styled Component Classes
